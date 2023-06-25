35919e7d4a836662c0bfa0d467baff2f3cb6d44c
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-06-25 12:26:11 +0200

    Change font to PragmataPro

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "PragmataPro:pixelsize=16:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
